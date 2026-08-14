4686ba3477b4a6b8da315faa102d536cca490d4a
SONALI YADAV <146146467+Sonali-web-dev@users.noreply.github.com>
2026-08-14 11:22:21 +0530

    Time: 54 ms (27.2%), Space: 61.8 MB (38.43%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool canPlace(vector<int>& position, int m, int minDist) {
-        int count = 1;                 // First ball is placed
-        int last = position[0];        // Position of last placed ball
-
+        int count = 1;                 
+        int last = position[0];      
         for (int i = 1; i < position.size(); i++) {
 
-            // If distance is at least minDist,
-            // place another ball here
+       
             if (position[i] - last >= minDist) {
                 count++;
                 last = position[i];
             }
 
-            // We successfully placed all m balls
+            
             if (count >= m) {
                 return true;
             }
@@ -36,14 +34,12 @@ public:
             int mid = low + (high - low) / 2;
 
             if (canPlace(position, m, mid)) {
-                // mid is possible
-                // Try to get a larger minimum distance
+              
                 ans = mid;
                 low = mid + 1;
             }
             else {
-                // mid is not possible
-                // Need a smaller distance
+                
                 high = mid - 1;
             }
         }
